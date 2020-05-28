87a353014f606aafe03781144106bf7249db1a81
Muhammad Taha <37846193+AbsorbedInThought@users.noreply.github.com>
2020-05-28 22:41:37 +0500

    Update main.py

--- python-synthesis/main.py
+++ python-synthesis/main.py
@@ -1,42 +1,61 @@
 #Libraries
-import json
-import threading
-from importlib import import_module
+import multiprocessing
+import numpy as np
+import ctypes
 from classes.wire import Wire
-from modules.edge_detector import edge_detector
-from modules.camera import camera
-from modules.screen import screen
-
-#Configurations
-WIRES_PATH = "example/wires.json"
-MAPPING_PATH = "example/mapping.json"
 
 #Functions
 def load_JSON(file_path):
 
+    import json
     with open(file_path) as obj:
         data = json.load(obj)
     return data
 
-wires = load_JSON(WIRES_PATH)
+def initialize():
 
-wires_list = []
-for wire in wires:
-    wires_list.append(Wire(wire))
+    import argparse
+    parser = argparse.ArgumentParser(description='Python Synthesizer')
+    parser.add_argument('--wires', help='Enter path to wires.json', default="example/wires.json")
+    parser.add_argument('--mapping', help='Enter path to mapping.json', default="example/mapping.json")
+    args = parser.parse_args()
 
-data = load_JSON(MAPPING_PATH)
+    wires = load_JSON(args.wires)
+    data = load_JSON(args.mapping)
 
-thread_pool = []
+    return wires, data
 
-for i, element in enumerate(data["mapping"]):
-    method_name = element["block_name"]
-    possibles = globals().copy()
-    possibles.update(locals())
-    method = possibles.get(method_name)
-    input_args = element["input_wires"]
-    output_args = element["output_wires"]
-    parameters = element["parameters"]
-    print(method_name, input_args, output_args, parameters)
-    thread_pool.append(threading.Thread(target=method,
-    args=(wires_list, input_args, output_args, parameters,)))
-    thread_pool[i].start()
+def shared_array(shape):
+    shared_array_base = multiprocessing.Array(ctypes.c_uint8, shape[0]*shape[1])
+    shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
+    shared_array = shared_array.reshape(*shape)
+    return shared_array
+
+if __name__ == "__main__":
+
+    wires, data = initialize()
+
+    wires_list = []
+    for wire in wires:
+        wires_list.append( Wire(wire, shared_array((480, 640)), multiprocessing.Lock()) )
+
+    processes = []
+    for i, element in enumerate(data["mapping"]):
+
+        from importlib import import_module
+        method_name = 'modules.' + element["block_name"] + '.' + element["block_name"]
+        module, function = method_name.rsplit('.',1)
+        mod = import_module(module)
+        method = getattr(mod, function)
+
+        input_args = element["input_wires"]
+        output_args = element["output_wires"]
+        parameters = element["parameters"]
+
+        processes.append(multiprocessing.Process(target=method,
+        args=(wires_list, input_args, output_args, parameters,)))
+
+        processes[i].start()
+
+    for process in processes:
+        process.join()
