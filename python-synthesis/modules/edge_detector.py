f4d2fe090b7f91a4b3bf01de8a15b32425f4903e
Muhammad Taha <37846193+AbsorbedInThought@users.noreply.github.com>
2020-05-28 22:45:11 +0500

    Update edge_detector.py

--- python-synthesis/modules/edge_detector.py
+++ python-synthesis/modules/edge_detector.py
@@ -2,12 +2,12 @@ import cv2 as cv
 
 def edge_detector(wires_list, input_wires, output_wires, parameters):
 
-    input_wire_1 = input_wires[0]
-    output_wire_1 = output_wires[0]
+    input_wire_1 = [wire for wire in wires_list if input_wires[0] == wire.id][0]
+    output_wire_1 = [wire for wire in wires_list if output_wires[0] == wire.id][0]
     parameter_1 = int(parameters[0])
 
     while(True):
-        img = wires_list[int(input_wire_1)].read()
+        img = input_wire_1.read()
         if img is not None:
-            img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            wires_list[int(output_wire_1)].write(cv.Canny(img, 100, 200, 100, parameter_1))
+            #img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            output_wire_1.write(cv.Canny(img, 100, 200, 100, parameter_1))
