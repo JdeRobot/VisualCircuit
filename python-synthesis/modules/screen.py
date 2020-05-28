d6904aae321c947ddbd536edcaa0da9983fab715
Muhammad Taha <37846193+AbsorbedInThought@users.noreply.github.com>
2020-05-28 22:45:36 +0500

    Update screen.py

--- python-synthesis/modules/screen.py
+++ python-synthesis/modules/screen.py
@@ -2,10 +2,10 @@ import cv2 as cv
 
 def screen(wires_list, input_wires, output_wires, parameters):
 
-    input_wire_1 = input_wires[0]
+    input_wire_1 = [wire for wire in wires_list if input_wires[0] == wire.id][0]
 
     while(True):
-        img = wires_list[int(input_wire_1)].read()
+        img = input_wire_1.read()
 
         if img is not None:
             cv.imshow("Monitor", img)
