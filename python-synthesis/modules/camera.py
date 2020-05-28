c00764cd4d8334d87fca4989ffb812ac81149b4e
Muhammad Taha <37846193+AbsorbedInThought@users.noreply.github.com>
2020-05-28 22:44:48 +0500

    Update camera.py

--- python-synthesis/modules/camera.py
+++ python-synthesis/modules/camera.py
@@ -2,9 +2,11 @@ import cv2 as cv
 
 def camera(wires_list, input_wires, output_wires, parameters):
 
-   output_wire_1 = output_wires[0]
    cap = cv.VideoCapture(0)
+
+   output_wire_1 = [wire for wire in wires_list if output_wires[0] == wire.id][0]
 
    while(True):
         ret, frame = cap.read()
-        wires_list[int(output_wire_1)].write(frame)
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        output_wire_1.write(frame)
