fb8f67387f8b976cddf1003fba0aa8625da3c11e
toshan-luktuke <toshan1603@gmail.com>
2022-07-16 16:15:23 +0530

    Add general purpose read and share functions

--- backend/staticfiles/synthesis/lib/inputs.py
+++ backend/staticfiles/synthesis/lib/inputs.py
@@ -17,6 +17,43 @@ class Inputs:
     def __init__(self, input_data) -> None:
         self.inputs = input_data
 
+    def read(self, name):
+        if self.inputs.get(name) is None:
+            raise InvalidInputNameException(f"{name} is not declared in inputs")
+
+        if self.inputs[name].get("created", False):
+            dim = create_ndbuffer((1,), np.int64, self.inputs[name]["dim"].buf)[:][0]
+            shape = create_ndbuffer((dim,), np.int64, self.inputs[name]["shape"].buf)
+            type = create_ndbuffer((1,), '<U6', self.inputs[name]["type"].buf)[:][0]
+            data = create_ndbuffer(shape, type, self.inputs[name]["data"].buf)
+        else:
+            wire_name = self.inputs[name]["wire"]
+            data_wire = create_readonly_wire(wire_name)
+            dim_wire = create_readonly_wire(wire_name + "_dim")
+            shape_wire = create_readonly_wire(wire_name + "_shape")
+            type_wire = create_readonly_wire(wire_name + "_type")
+
+            if data_wire is None or shape_wire is None or dim_wire is None or type_wire is None:
+                return None
+
+            self.inputs[name]["dim"] = dim_wire
+            dim = create_ndbuffer((1,), np.int64, dim_wire.buf)[:][0]
+
+            self.inputs[name]["type"] = type_wire
+            type = create_ndbuffer((1,), '<U6', type_wire.buf)[:][0]
+            if not type:
+                return None 
+
+            self.inputs[name]["shape"] = shape_wire
+            shape = create_ndbuffer((dim,), np.int64, shape_wire.buf)
+
+            self.inputs[name]["data"] = data_wire
+            data = create_ndbuffer(shape, type[:], data_wire.buf)
+            
+            self.inputs[name]["created"] = True
+
+        return data
+
     def _read_npy_matrix(self, name, dtype):
         if self.inputs[name].get("created", False):
             dim = create_ndbuffer((1,), np.int64, self.inputs[name]["dim"].buf)[:][0]
--- backend/staticfiles/synthesis/lib/outputs.py
+++ backend/staticfiles/synthesis/lib/outputs.py
@@ -15,6 +15,59 @@ class Outputs:
         self.shms.append(shm)
         return shm
 
+    def check_type(self, typestr):
+
+        chars = int(typestr[2:])
+        if typestr[1] == 'i' or typestr[1] == 'f':
+            suffix_no = chars if chars > 8 else 8
+        elif typestr[1] == 'U' or typestr[1] == 'S':
+            suffix_no = chars if chars > 64 else 64
+        else:
+            suffix_no = chars
+
+        final_type = typestr[:2] + str(suffix_no)
+        return (final_type)
+
+    def share(self, name, data):
+        if self.outputs.get(name) is None:
+            raise InvalidOutputNameException(f"{name} is not declared in outputs")
+        
+        data = np.array(data)
+        shape = np.array(data.shape)
+        dim = np.array([len(shape)])
+        final_type = self.check_type(data.dtype.str)
+        type = np.array([final_type], dtype='<U6')
+        
+        if self.outputs[name].get("created", False):
+            self.outputs[name]["shape"][:] = shape[:]
+            self.outputs[name]["type"][:] = type[:]
+            self.outputs[name]["data"][:] = data[:]
+
+        else:
+            wire_name = self.outputs[name]["wire"]
+            shape_wire = self._create_wire(wire_name + "_shape", shape.nbytes)
+            dim_wire = self._create_wire(wire_name + "_dim", dim.nbytes) 
+            type_wire = self._create_wire(wire_name + "_type", type.nbytes)
+
+            data_size = data.nbytes if data.nbytes > 256 else 256 
+            data_wire = self._create_wire(self.outputs[name]["wire"], data_size)
+
+            self.outputs[name]["dim"] = create_ndbuffer((1,), np.int64, dim_wire.buf)
+            self.outputs[name]["dim"][:] = dim[:]
+
+            self.outputs[name]["type"] = create_ndbuffer((1,), '<U6', type_wire.buf)            
+            self.outputs[name]["type"][:] = type
+
+            self.outputs[name]["shape"] = create_ndbuffer(
+                shape.shape, shape.dtype, shape_wire.buf
+                )
+            self.outputs[name]["shape"][:] = shape[:]
+
+            self.outputs[name]["data"] = create_ndbuffer(shape, type[0], data_wire.buf)
+            self.outputs[name]["data"][:] = data
+        
+            self.outputs[name]["created"] = True
+
     def _share_npy_matrix(self, name, matrix, shape):
         dim = np.array([len(matrix.shape)], dtype=np.int64)
         if self.outputs[name].get("created", False):
