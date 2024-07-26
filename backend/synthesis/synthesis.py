3afffd77ab40766c36ebbbcb1bb603feb38b70eb
BkPankaj <borade.keshav825@gmail.com>
2024-07-27 02:00:22 +0530

    added working recurssion method for generating nested modules

--- backend/synthesis/synthesis.py
+++ backend/synthesis/synthesis.py
@@ -25,7 +25,7 @@ def get_number_or_default(num, default):
 
 def syntheize_modules(data: dict, zipfile: InMemoryZip) -> Tuple[InMemoryZip, Dict[str, bool]]:
     '''Synthesize python code for different blocks as well as user code blocks.
-    Different blocks present in the project is collected.
+    Different blocks present in the project are collected.
     Parameters of each dependency block as well as constant blocks are collected.
     Blocks, parameters and the connections (wires) between the blocks stored in a 
     JSON file.
@@ -35,76 +35,76 @@ def syntheize_modules(data: dict, zipfile: InMemoryZip) -> Tuple[InMemoryZip, Di
     parameters = {}
     synhronize_frequency = {}
     optional_files = {}
+    dep_no = {} 
 
-    dep_no = {} # Dictionary to store the number of blocks of each type
+    def process_dependency(dep, zipfile, synhronize_frequency, optional_files, dep_no, parameters, dependencies):
+        
+        for key, dependency in dep.items():
+            components = dependency['design']['graph']['blocks']
+            print("dependency['design']['graph']['blocks']",dependency['design']['graph']['blocks'])
+            for block in components:
+                block_id = block['id']
+                if block['type'] == 'basic.code':
+                    script = block['data']['code']
+                    script_name = dependency['package']['name']
+                    synhronize_frequency[key] = get_number_or_default(block['data'].get('frequency', 30), 30)
+                    
+                    if script_name in optional_files:
+                        optional_files[script_name] = True
 
-    for key, dependency in data['dependencies'].items():
+                    script_name += dependency['package']['version'].replace('.', '')
+                    
+                    if script_name in dep_no:
+                        dep_no[script_name] += 1
+                    else:
+                        dep_no[script_name] = 1
 
-        components = dependency['design']['graph']['blocks']
+                    script_name += str(dep_no[script_name])
+                    dependencies[key] = script_name
 
-        for block in components:
-            if block['type'] == 'basic.code':
-                # If code, generate python file.
-                script = block['data']['code']
-                # The file name is stored in script_name here
-                script_name = dependency['package']['name'] 
-                synhronize_frequency[key] = get_number_or_default(block['data'].get('frequency', 30), 30)
-                # Mark the optional files required by the current block, if needed.
-                if script_name in OPTIONAL_FILES:
-                    optional_files[script_name] = True
+                    zipfile.append(f'{BLOCK_DIRECTORY}/{script_name}.py', script)
+                elif block['type'] == 'basic.constant':
+                    parameters[key] = parameters.get(key, [])
+                    parameters[key].append({
+                        'id': block['id'],
+                        'name': block['data']['name'], 
+                        'value': block['data']['value']}
+                    )
 
-                # The version name is simply added to the name of the file 
-                script_name += dependency['package']['version'].replace('.', '') 
+            if 'dependencies' in dependency and dependency['dependencies']:
+                process_dependency(dependency['dependencies'], zipfile, synhronize_frequency, optional_files, dep_no, parameters, dependencies)
 
-                # If the block already exists, then add 1 to the number to be appended to it 
-                if script_name in dep_no:
-                    dep_no[script_name] += 1
-                else:
-                    dep_no[script_name] = 1
-
-                # Add the block number to the end of the block's name
-                script_name += str(dep_no[script_name])
-
-                dependencies[key] = script_name
-
-                # Python file is created
-                zipfile.append(f'{BLOCK_DIRECTORY}/{script_name}.py', script)
-
-            elif block['type'] == 'basic.constant':
-                # If constant, it is a parameter.
-                # Since this is a parameter for a dependency block we make the dependency type block
-                # as the key. This is later used to identify which parameter belongs to which dependency
-                # block
-                parameters[key] = parameters.get(key, [])
-                parameters[key].append({
-                    'id': block['id'],
-                    'name': block['data']['name'], 
-                    'value': block['data']['value']}
-                )
+    # Process the top-level dependencies
+    if 'dependencies' in data:
+        process_dependency(data['dependencies'], zipfile, synhronize_frequency, optional_files, dep_no, parameters, dependencies)
 
     count = 1
+    print("depend ba",data['design']['graph']['blocks'])
     for block in data['design']['graph']['blocks']:
-        block_id, block_type = block['id'], block['type']
-        if block_type == 'basic.code':
-            # If code, generate python file.
-            code_name = "Code_"+str(count)
-            count += 1
-            script = block['data']['code']
-            synhronize_frequency[block_id] = get_number_or_default(block['data'].get('frequency', 30), 30)
-            zipfile.append(f'{BLOCK_DIRECTORY}/{code_name}.py', script)
-            blocks[block_id] = {'name': code_name, 'type': block_type}
-        elif block_type == 'basic.constant':
-            # If constant, it is a parameter.
-            # Since this is a parameter at project level, we make the key as constant block ID
-            parameters[block_id] = [{'name': block['data']['name'], 'value': block['data']['value']}]
-        else:
-            # TODO: Check how input and output blocks behave.
-            # This behaviour is for Package blocks only
-            blocks[block_id] = {'name': dependencies[block_type], 'type': block_type}
-            
+        print("depend b",dependencies)
+        print("block intern",block)
+        if 'source' not in block and 'target' not in block:
+            block_id, block_type = block['id'], block['type']
+            if block_type == 'basic.code':
+                code_name = "Code_" + str(count)
+                count += 1
+                script = block['data']['code']
+                synhronize_frequency[block_id] = get_number_or_default(block['data'].get('frequency', 30), 30)
+                zipfile.append(f'{BLOCK_DIRECTORY}/{code_name}.py', script)
+                blocks[block_id] = {'name': code_name, 'type': block_type}
+            elif block_type == 'basic.constant':
+                parameters[block_id] = [{'name': block['data']['name'], 'value': block['data']['value']}]
+            else:
+                blocks[block_id] = {'name': dependencies.get(block_type, ''), 'type': block_type}
+                print("blocks[block_id]",blocks[block_id])
+                print("depend",dependencies)
 
-
-    data = {'blocks': blocks, 'parameters': parameters, 'synchronize_frequency': synhronize_frequency, 'wires': data['design']['graph']['wires']}
+    data = {
+        'blocks': blocks, 
+        'parameters': parameters, 
+        'synchronize_frequency': synhronize_frequency, 
+        'wires': data['design']['graph']['wires']
+    }
     zipfile.append('data.json', json.dumps(data))
 
     return zipfile, optional_files
