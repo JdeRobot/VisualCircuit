52b8f64e7fd9584aee03a69d918856c421416f46
Sarvesh Mishra <sarveshmishraiit1981@gmail.com>
2026-07-23 19:19:47 +0530

    feat: implement django api for custom blocks

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('build', views.build, name='build'),
     path('download', views.download, name='download'),
+    path('install_block', views.install_block, name='install_block'),
+    path('installed_blocks', views.installed_blocks, name='installed_blocks'),
 ]
